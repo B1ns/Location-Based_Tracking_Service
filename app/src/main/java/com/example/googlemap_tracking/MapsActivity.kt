d41d5bb4c45dae30d32991a2b7fbe588495c3b30
B1ns <qlsl3574@naver.com>
2019-05-23 15:58:02 +0900

    code test

--- app/src/main/java/com/example/googlemap_tracking/MapsActivity.kt
+++ app/src/main/java/com/example/googlemap_tracking/MapsActivity.kt
@@ -1,8 +1,12 @@
 package com.example.googlemap_tracking
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,6 +18,10 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -47,7 +55,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
-    private fun locationInit(){
+    private fun locationInit() {
         fusedLocationProviderClient = FusedLocationProviderClient(this)
 
         locationCallback = MyLocationCallback()
@@ -59,7 +67,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // 위치 정보가 없을때는 업데이트 안 함
         locationRequest.interval = 10000
 
+        // 정확함. 이것보다 짧은 업데이트는 하지 않음
+        locationRequest.fastestInterval = 5000
 
+
+    }
+
+    private fun showPermissionInfoDialog() {
+        alert("현재 위치 정보를 얻기 위해서 위치 권한이 필요합니다.", "권한이 필요한 이유") {
+            yesButton {
+                ActivityCompat.requestPermissions(
+                    this@MapsActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_ACCESS_FINE_LOCATION
+                )
+            }
+
+            noButton {
+                toast("거절")
+            }
+        }.show()
     }
 
     override fun onResume() {
@@ -67,27 +93,67 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         addLocationListener()
     }
 
-    private fun addLocationListener(){
-
+    override fun onPause() {
+        super.onPause()
+        removeLocationListener()
     }
 
-    inner class MyLocationCallback : LocationCallback(){
+    private fun addLocationListener() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    inner class MyLocationCallback : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult?) {
             super.onLocationResult(locationResult)
 
             val location = locationResult?.lastLocation
 
-            location?.run{
+            location?.run {
 
                 val latLng = LatLng(latitude, longitude)
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
 
                 Log.d("MapsActivity", "위도 : $latitude, 경도 : $longitude")
 
-
             }
         }
     }
 
+    private fun removeLocationListener() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
 
+    private fun permissionCheck(cancel: () -> Unit, ok: () -> Unit) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                cancel()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_ACCESS_FINE_LOCATION
+                )
+            }
+        } else {
+            ok()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_ACCESS_FINE_LOCATION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    addLocationListener()
+                } else {
+                    //권한 거부
+                    toast("권한 거부 됨")
+                }
+                return
+            }
+        }
+    }
 }
