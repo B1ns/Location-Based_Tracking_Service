1bc7d26be9106e9c3bb19cba7edb2cdb4385610f
B1ns <qlsl3574@naver.com>
2019-05-23 16:07:46 +0900

    Completion of line drawing and map service

--- app/src/main/java/com/example/googlemap_tracking/MapsActivity.kt
+++ app/src/main/java/com/example/googlemap_tracking/MapsActivity.kt
@@ -1,10 +1,14 @@
 package com.example.googlemap_tracking
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -14,6 +18,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
@@ -29,6 +34,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private val REQUEST_ACCESS_FINE_LOCATION = 1000
 
+    private val polylineOptions = PolylineOptions().width(5f).color(Color.RED)
+
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: MyLocationCallback
@@ -37,15 +44,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
+        // 화면이 꺼지지 않게 하기
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         //SupportMapFragment를 가져와서 지도가 준비되면 알림을 받습니다.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        // 세모 모드로 화면 고정
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        setContentView(R.layout.activity_maps)
+
         locationInit()
     }
 
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -53,6 +68,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        permissionCheck(cancel = {
+            showPermissionInfoDialog()
+        }, ok = {
+            mMap.isMyLocationEnabled = true
+        })
     }
 
     private fun locationInit() {
@@ -98,25 +119,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         removeLocationListener()
     }
 
+    @SuppressLint("MissingPermission")
     private fun addLocationListener() {
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
-    }
-
-    inner class MyLocationCallback : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult?) {
-            super.onLocationResult(locationResult)
-
-            val location = locationResult?.lastLocation
-
-            location?.run {
-
-                val latLng = LatLng(latitude, longitude)
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
-
-                Log.d("MapsActivity", "위도 : $latitude, 경도 : $longitude")
-
-            }
-        }
     }
 
     private fun removeLocationListener() {
@@ -153,6 +158,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     toast("권한 거부 됨")
                 }
                 return
+            }
+        }
+    }
+
+    inner class MyLocationCallback : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            val location = locationResult?.lastLocation
+
+            location?.run {
+
+                val latLng = LatLng(latitude, longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
+
+                Log.d("MapsActivity", "위도 : $latitude, 경도 : $longitude")
+
+                polylineOptions.add(latLng)
+                //선 그리기
+                mMap.addPolyline(polylineOptions)
             }
         }
     }
