34096c31e4594f05ecd07a3c64c89b08f74df7e9
B1ns <qlsl3574@naver.com>
2019-05-23 10:03:52 +0900

    Maps latitude & longitude check

--- app/src/main/java/com/example/googlemap_tracking/MapsActivity.kt
+++ app/src/main/java/com/example/googlemap_tracking/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.example.googlemap_tracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,18 +10,31 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private val REQUEST_ACCESS_FINE_LOCATION = 1000
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: MyLocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        //SupportMapFragment를 가져와서 지도가 준비되면 알림을 받습니다.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationInit()
     }
 
 
@@ -32,4 +46,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    private fun locationInit(){
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        locationCallback = MyLocationCallback()
+
+        locationRequest = LocationRequest()
+        //GPS 우선
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        // 업데이트 인터벌
+        // 위치 정보가 없을때는 업데이트 안 함
+        locationRequest.interval = 10000
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        addLocationListener()
+    }
+
+    private fun addLocationListener(){
+
+    }
+
+    inner class MyLocationCallback : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            val location = locationResult?.lastLocation
+
+            location?.run{
+
+                val latLng = LatLng(latitude, longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
+
+                Log.d("MapsActivity", "위도 : $latitude, 경도 : $longitude")
+
+
+            }
+        }
+    }
+
+
 }
